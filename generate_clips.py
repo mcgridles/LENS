9ca1b68fe1909fa64467edd8bdb5c08aed8e9e4d
Henry Gridley <henry.gridley@gmail.com>
2019-10-28 23:56:42 -0400

    feat: Keep clips from different views together

--- generate_clips.py
+++ generate_clips.py
@@ -4,6 +4,7 @@ import cv2
 import argparse
 import numpy as np
 import glob
+from collections import defaultdict
 from utils import flow_parser, parse_flow_args
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,33 +13,37 @@ from optical_flow import OpticalFlow
 from optical_flow import models, losses, tools
 
 
-def generate_clips(video_path, output_dir, duration, flow):
+def generate_clips(video_name, video_path, output_dir, duration, flow, start_idx=0):
     """
     Generate random clips from video file.
 
+	:param video_name: (str) -> Base name of video
     :param video_path: (str) -> Path to the video to be split
     :param output_dir: (str) -> Path to directory to save clips in
     :param duration: (int) -> Duration of the new split videos in seconds
     :param flow: (np.ndarray) -> U and V channels of the optical flow calculated over the video
+    :param start_idx: (int) -> Starting index for clip numbering
+    :return: (int) -> Highest clip index for the current action/group configuration
     """
 
     rgb, fps = load_video(video_path)
     assert len(rgb) == len(flow[0])
     assert len(rgb) == len(flow[1])
 
-    video_name = os.path.splitext(os.path.basename(video_path))[0]
     chunks = int(rgb.shape[0] / (fps * duration))
 
     rgb_dir = os.path.join(output_dir, 'rgb')
     of_u_dir = os.path.join(output_dir, 'flownet2', 'u')
     of_v_dir = os.path.join(output_dir, 'flownet2', 'v')
 
-    chunk_and_save(rgb, chunks, video_name, rgb_dir)
+    max_clip_idx = chunk_and_save(rgb, chunks, video_name, rgb_dir)
     chunk_and_save(flow[0], chunks, video_name, of_u_dir)
     chunk_and_save(flow[1], chunks, video_name, of_v_dir)
 
+    return max_clip_idx
 
-def chunk_and_save(video, chunks, video_name, output_dir):
+
+def chunk_and_save(video, chunks, video_name, output_dir, start_idx=0):
     """
     Separates video into chunks and saves the frames.
 
@@ -46,12 +51,16 @@ def chunk_and_save(video, chunks, video_name, output_dir):
     :param chunks: (int) -> Number of chunks to divide video into
     :param video_name: (str) -> Name of video file
     :param output_dir: (str) -> Path to output directory
+    :param start_idx: (int) -> Starting index for clip numbering
+    :return: (int) -> Highest clip index for the current action/group configuration
     """
 
     clips = np.array_split(video, chunks, 0)
+   	max_clip_idx = 0
     for i, clip in enumerate(clips):
-        clip_num = '_c{}'.format(str(i+1).zfill(6))
+        clip_num = '_c{}'.format(str(i + start_idx).zfill(6))
         clip_name = video_name + clip_num
+        max_clip_idx = i + start_idx
 
         # Create directory if it doesn't exist
         dir_name = os.path.join(output_dir, clip_name)
@@ -70,6 +79,8 @@ def chunk_and_save(video, chunks, video_name, output_dir):
                                      clip_name, 
                                      'frame{}.jpg'.format(str(frame_num+1).zfill(6)))
             cv2.imwrite(clip_path, frame)
+
+    return max_clip_idx
 
 
 def load_video(video_path):
@@ -153,15 +164,27 @@ def main():
     """
     Example:
     >>> python generate_clips -v /path/to/video.mov -o /path/to/directory -d 3
+
+    Video naming scheme: v_<ACTION>_g<XX>_v<Y>_<Z>
+    	ACTION = Name of action
+    	XX     = Two digit group number
+    	Y      = Clip number (for when multiple videos of same group)
+    	Z      = Letter denoting camera view (i.e. a, b, c, etc.)
     """
 
     args = parse_args()
     of = OpticalFlow(args)
     flow = generate_flow(of, args.video)
 
+    video_record = defaultdict(int)
     video_files = glob.glob(os.path.join(args.video, '*{}'.format(args.ext)))
-    for video in video_files:
-        generate_clips(video, args.output, args.duration, flow)
+    for video_path in video_files:
+    	video_name = os.path.splitext(os.path.basename(video_path))[0]
+    	video_name = video_name[:-5]
+    	start_idx = video_map[video_name] + 1
+
+        max_clip_idx = generate_clips(video_name, video_path, args.output, args.duration, flow, start_idx)
+        video_record[video_name] = max_clip_idx
 
 
 if __name__ == '__main__':
