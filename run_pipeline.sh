341b9637018756910f7a4395b4f3c0848c3bf522
Henry Gridley <henry.gridley@gmail.com>
2019-12-01 14:41:04 -0500

    chore: Remove unused script
    This script has never been used and an example for how to run the pipeline is included as a docstring comment in `pipeline.py`.

--- run_pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python pipeline.py --stream /mnt/disks/datastorage/videos/keyboard_cat.mp4 \
-				   --optical_weights /mnt/disks/datastorage/weights/optical_weights.pth.tar \
-				   --spatial_weights /mnt/disks/datastorage/weights/spatial_weights.pth.tar \
-				   --motion_weights /mnt/disks/datastorage/weights/motion_weights.pth.tar
