8c9ac04ff8b69f45f49c4099d8115eb45a95527a
Henry Gridley <henry.gridley@gmail.com>
2019-12-01 20:15:52 -0500

    chore: Change default buffer size in run script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,6 +3,7 @@
 python -W ignore pipeline.py --stream /mnt/disks/datastorage/videos/cold_day/demos/shooting_ballroom_light6.mov \
                              --nb_classes 3 \
                              --skip_frames 1 \
+                             --buffer_size 100 \
                              --svm /home/mlp/two-stream-action-recognition/demos_svm.pkl \
                              --save /mnt/disks/datastorage/predictions/ \
                              --model FlowNet2CSS \
