2c3d23f6850a4476bae114e9357655411b61b557
Henry Gridley <henry.gridley@gmail.com>
2019-12-01 19:33:03 -0500

    fix: Use correct SVM file

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,7 +3,7 @@
 python -W ignore pipeline.py --stream /mnt/disks/datastorage/videos/ballroom/v_Misc_g29_v1_b.mov \
                              --nb_classes 3 \
                              --skip_frames 1 \
-                             --svm /home/mlp/two-stream-action-recognition/demo_svm.pkl \
+                             --svm /home/mlp/two-stream-action-recognition/demos_svm.pkl \
                              --save /mnt/disks/datastorage/predictions/ \
                              --model FlowNet2CSS \
                              --optical_weights /mnt/disks/datastorage/weights/FlowNet2-CSS.pth.tar \
